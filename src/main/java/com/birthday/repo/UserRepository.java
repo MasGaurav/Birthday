be89a8ca6d98d14a5b3f2afb7f2bbe31e0995c54
Gaurav <lit2017050@iiitl.ac.in>
2021-08-13 15:49:44 +0530

    First commit

---
+++ src/main/java/com/birthday/repo/UserRepository.java
@@ -0,0 +1,8 @@
+package com.birthday.repo;
+
+import com.birthday.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    public User findByUsername(String username);
+}
